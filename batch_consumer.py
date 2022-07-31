6afdb4dd22b5576af667e0dd2140f93f383a8b64
sjumaa93 <sjumaa93@gmail.com>
2022-07-31 15:54:39 +0100

    updates

--- batch_consumer.py
+++ batch_consumer.py
@@ -1,9 +1,16 @@
 from kafka import KafkaConsumer
 from json import loads
+from json import dumps
 import json
 import boto3
+from numpy import insert
+from sqlalchemy import create_engine
 
 batch_consumer = KafkaConsumer('MyFirstKafkaTopic', bootstrap_servers = "localhost:9092")
+client = boto3.client('s3')
 
 for data in batch_consumer:
-    print(data)
+    with open("raw_data.json", "w") as f:
+        json.dump(data, f)
+
+#client.upload_file('raw_data.json', 'bucketnamehere', 'data.json')
