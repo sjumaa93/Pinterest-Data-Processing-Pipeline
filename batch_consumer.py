b40705735b99e5a07a5e34fc62f31f7c9ced9aae
sjumaa93 <sjumaa93@gmail.com>
2022-08-07 15:18:09 +0100

    update boto3

--- batch_consumer.py
+++ batch_consumer.py
@@ -1,3 +1,4 @@
+from time import sleep
 from kafka import KafkaConsumer
 from json import loads
 from json import dumps
@@ -6,11 +7,26 @@ import boto3
 from numpy import insert
 from sqlalchemy import create_engine
 
-batch_consumer = KafkaConsumer('MyFirstKafkaTopic', bootstrap_servers = "localhost:9092")
-client = boto3.client('s3')
+batch_consumer = KafkaConsumer(
+    'MyFirstKafkaTopic', bootstrap_servers = "localhost:9092",
+    value_deserializer = lambda pinmessage: loads(pinmessage), 
+    #auto_offset_reset = "earliest"
+    )
 
-for data in batch_consumer:
-    with open("raw_data.json", "w") as f:
-        json.dump(data, f)
+batch_consumer.subscribe(topics = "MyFirstKafkaTopic")
 
-#client.upload_file('raw_data.json', 'bucketnamehere', 'data.json')
+file_index = 0
+
+for message in batch_consumer:
+    file_index += 1
+    sleep(2)
+    file_name = 'data/data_' + str(file_index) + '.json'
+    with open(file_name, "w") as f:
+        json.dump(message, f, indent=2)
+
+
+    
+
+
+#s3 = boto3.resource('s3')
+#s3.meta.client.upload_file('./data.json','pinterest-data-bucket','data.json')
--- test.py
+++
@@ -1,3 +0,0 @@
-import findspark
-
-findspark.init()
